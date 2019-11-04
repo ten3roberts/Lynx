b1cdd5cabd8f3d4c975faae39e7a35ca2dd76a6e
Tim Roberts <ten3roberts@gmail.com>
2019-11-04 10:26:21 +0100

    Added GUID

---
+++ Lynx/src/GUID.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <pch.h>
+
+#include <bitset>
+namespace Lynx
+{
+	class LYNX_API GUID
+	{
+	private:
+		std::string m_value;
+	public:
+		GUID();
+		void Generate();
+		operator std::string()
+		{
+			return m_value;
+		}
+
+		bool operator==(const GUID& ID)
+		{
+			return ID.getString() == m_value;
+		}
+		//Generates a GUID complient with the RFC 4122 standard
+		std::string getString() const { return m_value; }
+	};
+}
